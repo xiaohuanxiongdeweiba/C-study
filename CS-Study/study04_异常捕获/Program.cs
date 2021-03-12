c0ac9eb6c654e65c56e413646b4c5f5450baa40d
yumengyang <yumengyang@fotoable.com>
2021-03-12 19:06:23 +0800

    no message

---
+++ CS-Study/study04_异常捕获/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace study04_异常捕获
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            /*try-catch
+             * 
+             * try
+             * {
+             * 可能会出现异常的代码；
+             * }
+             * catch
+             * {
+             * 出现异常后要执行的代码；
+             * }
+             * 
+             * 如果出现异常，后面的代码都不执行了，直接跳到catch。
+             * 
+             * 
+             * 变量的作用域
+             * 从声明变量的大括号开始，到大括号结束，是作用范围。
+             */
+        }
+    }
+}
