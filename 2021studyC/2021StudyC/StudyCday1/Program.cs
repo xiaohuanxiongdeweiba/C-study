e0eca1d94b0f0244c870b56f358b332f81ee082b
yumengyang <yumengyang@fotoable.com>
2021-12-06 16:46:14 +0800

    no message

---
+++ 2021studyC/2021StudyC/StudyCday1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StudyCday1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello,world!");
+            Console.ReadKey();
+        }
+    }
+}
+//.cs结尾的是类
+//解决方案 是公司   项目是部门   类是员工
+//sln 解决方案文件
+//csproj 项目文件
