e657d6335ebec5dd76b40668a8c12dc38a3120ba
yumengyang <yumengyang@fotoable.com>
2021-12-08 21:01:40 +0800

    no message

--- 2021studyC/2021StudyC/StudyCday2/Program.cs
+++ 2021studyC/2021StudyC/StudyCday2/Program.cs
@@ -4,9 +4,29 @@ namespace StudyCday2
 {
     class Program
     {
+        /// <summary>
+        /// 摘要，解释方法的作用
+        /// </summary>
+        /// <param name="args">参数</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //#region 多余代码
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //#endregion
+
+            //string n = "sdf";
+            //String ss = "sdfsdf";
+            //int sd = 100;
+
+            Console.WriteLine(@"Hello,
+World!");
+            Console.ReadKey();
+
+
+
         }
     }
 }
---
+++ 2021studyC/2021StudyC/StudyCday3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StudyCday3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("请输入您的成绩");
+                double score = Convert.ToInt32(Console.ReadLine());
+                if (score >= 90)
+                {
+                    Console.WriteLine("您的成绩是：A");
+                }
+                else if (score >= 80)
+                {
+                    Console.WriteLine("您的成绩是：B");
+                }
+                else if (score >= 70)
+                {
+                    Console.WriteLine("您的成绩是：C");
+                }
+                else if (score >= 60)
+                {
+                    Console.WriteLine("您的成绩是：D");
+                }
+                else
+                {
+                    Console.WriteLine("您的成绩是：F");
+                }
+            }
+
+
+
+
+        }
+    }
+}
