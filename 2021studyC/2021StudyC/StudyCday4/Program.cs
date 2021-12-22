07870a2d1c45901f7e7a3e6d693f1f4d4e98d3d3
yumengyang <yumengyang@fotoable.com>
2021-12-22 20:27:21 +0800

    no message

---
+++ 2021studyC/2021StudyC/StudyCday4/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace StudyCday4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            /*
+            Console.WriteLine("请输入年份");
+            string year = Console.ReadLine();
+            Console.WriteLine("请输入月份");
+            string month = Console.ReadLine();
+            
+            switch (Convert.ToDouble(month))
+            {
+                case 1:
+                    Console.WriteLine("该月的天数是 31 天");
+                    break;
+                case 2:
+                    if (Convert.ToDouble(year) % 4 == 0 || Convert.ToDouble(year) % 400 == 0)
+                    {
+                        Console.WriteLine("该月的天数是 29 天");
+                    }
+                    else
+                    {
+                        Console.WriteLine("该月的天数是 28 天");
+                    }
+                    
+                    break;
+                case 3:
+                    Console.WriteLine("该月的天数是 31 天");
+                    break;
+                case 4:
+                    Console.WriteLine("该月的天数是 30 天");
+                    break;
+                case 5:
+                    Console.WriteLine("该月的天数是 31 天");
+                    break;
+                case 6:
+                    Console.WriteLine("该月的天数是 30 天");
+                    break;
+                case 7:
+                    Console.WriteLine("该月的天数是 31 天");
+                    break;
+                case 8:
+                    Console.WriteLine("该月的天数是 31 天");
+                    break;
+                case 9:
+                    Console.WriteLine("该月的天数是 30 天");
+                    break;
+                case 10:
+                    Console.WriteLine("该月的天数是 31 天");
+                    break;
+                case 11:
+                    Console.WriteLine("该月的天数是 30 天");
+                    break;
+                case 12:
+                    Console.WriteLine("该月的天数是 31 天");
+                    break;
+
+            }*/
+            #endregion
+
+            int i = 100;
+            int sum = 0;
+            while (i > 0)
+            {
+                sum += i;
+                i--;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ 2021studyC/2021StudyC/StudyCday5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StudyCday5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //水仙花数
+            int a = 0;
+            int b = 0;
+            int c = 0;
+
+            for (int i = 100; i <= 999; i++)
+            {
+                a = i / 100;
+                b = (i - a * 100) / 10;
+                c = (i - a * 100 - b * 10);
+                int sum = a * a * a + b * b * b + c * c * c;
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+
+            }
+        }
+    }
+}
